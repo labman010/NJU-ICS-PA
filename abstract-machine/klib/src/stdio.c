1e88166f8efbded86db6862db2c232c9f4360f8b
YYH-father <756304353@qq.com>
2024-04-13 23:35:38 +0800

    Finish pa2.2 part1
        1.Add batch flag in makefile
        2.Implement some basic function in klib

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,11 +10,62 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char buf[20];
+  char *str, *s;
+  int d;
+
+  char *p = out;
+
+  while (*fmt) {
+    if (*fmt == '%') {
+      switch (*(++fmt)) {
+        case 's':
+          str = va_arg(ap, char *);
+          while (*str) {
+            *(p++) = *(str++);
+          }
+          break;
+        case 'd':
+          d = va_arg(ap, int);
+          s = buf;
+          if (d < 0) {
+            *(p++) = '-';
+            d = -d;
+          }
+          do {
+            *(s++) = '0' + (d % 10);
+            d /= 10;
+          } while (d);
+          // 倒序写入到输出字符串
+          while (s > buf) {
+            *(p++) = *(--s);
+          }
+          break;
+        default:
+          panic("Other format in vsprintf not implemented");
+          *(p++) = *fmt;
+          break;
+      }
+    } else {
+      *(p++) = *fmt;
+    }
+    ++fmt;
+  }
+  *p = '\0';
+  return p - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  int ret;
+
+  va_start(args, fmt);
+
+  ret = vsprintf(out, fmt, args);
+
+  va_end(args);
+
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,115 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+// 部分参考Linux标准库glibc源码
+
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t length = 0;
+  while (*s != '\0') {
+    length++;
+    s++;
+  }
+  return length;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  // strcpy本身就不是一个很安全的函数
+  char *ptr = dst;
+  while (*src != '\0') {
+    *dst = *src;
+    src++;
+    dst++;
+  }
+  *dst = '\0';
+  return ptr;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; i++) {
+    dst[i] = src[i];
+  }
+  for (; i < n; i++) {
+    dst[i] = '\0';
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t dst_len = strlen(dst);
+  size_t i;
+  for (i = 0; src[i] != '\0'; i++) {
+    dst[dst_len + i] = src[i];
+  }
+  dst[dst_len + i] = '\0';
+
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+    while (*s1 != '\0' && (*s1 == *s2)) {
+      s1++;
+      s2++;
+    }
+    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i = 0;
+  while (i < n && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]) {
+    i++;
+  }
+  if (i == n) {
+    return 0;
+  }
+  return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char *ptr = s;
+  unsigned char value = c;
+  for (size_t i = 0; i < n; i++) {
+    ptr[i] = value;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  unsigned char *pdst = dst;
+  const unsigned char *psrc = src;
+  if (pdst < psrc) {
+    for (size_t i = 0; i < n; i++) {
+      pdst[i] = psrc[i];
+    }
+  }
+  else {
+    for (size_t i = n; i > 0; i--) {
+      pdst[i - 1] = psrc[i - 1];
+    }
+  }
+
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  unsigned char *d = out;
+  const unsigned char *s = in;
+  while (n--) {
+    *d++ = *s++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *p1 = s1, *p2 = s2;
+  while (n-- > 0) {
+    if (*p1 != *p2) {
+        return *p1 - *p2;
+    }
+    p1++;
+    p2++;
+  }
+  return 0;
 }
 
 #endif
