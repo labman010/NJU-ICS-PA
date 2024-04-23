cd0c7b0d5f535a02fde145f7f6ae95b3f4a84790
YYH-father <756304353@qq.com>
2024-04-23 10:12:57 +0800

    Finish pa2.3 of IOE about keyboard and timer
        1.keyboard and timer
        2.add a todo comment for probable future work for dtrace

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -54,6 +54,9 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
+
+// [TODO] dtrace 每次访问设备的地址时,输出或记录信息
+
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
@@ -63,6 +66,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
+
+// [TODO] dtrace 每次访问设备的地址时,输出或记录信息
+
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
