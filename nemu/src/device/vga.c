a4636808049601bfb72742e337120fb8af20e394
YYH.ser5max <756304353@qq.com>
2024-04-28 22:21:40 +0800

    Finish: pa2.3 IOE about VGA

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -74,6 +74,10 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+  if (vgactl_port_base[1]) {  // 对应地址 (0xa0000100 + 4)
+    update_screen();
+    vgactl_port_base[1] = 0;
+  }
 }
 
 void init_vga() {
